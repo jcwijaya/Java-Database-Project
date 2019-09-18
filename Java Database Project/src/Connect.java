dee7e7ff5aad83cab625cda8f98229c138118b7f
Jessica Wijaya <Jessica Wijaya@DESKTOP-LS24TNU>
2019-09-18 11:09:40 -0500

    Added class Connect

--- Java Database Project/src/Connect.java
+++ Java Database Project/src/Connect.java
@@ -1,10 +1,11 @@
 
+//In this program we will connect to the database
 import java.sql.*;
 
 public class Connect {
 
 	public static void main(String[] args) {
-		Connection connection = new connection();
+		
 
 	}
 
