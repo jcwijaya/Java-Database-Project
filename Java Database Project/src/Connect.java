fca2a302a3fc7cfb39dc68cb2f8364487dcae018
Jessica Wijaya <Jessica Wijaya@DESKTOP-LS24TNU>
2019-09-18 12:00:50 -0500

    Updated class Connect

--- Java Database Project/src/Connect.java
+++ Java Database Project/src/Connect.java
@@ -1,5 +1,7 @@
 
-//In this program we will connect to the database
+/*In this program we will connect to the database Test and
+ * test how to execute JDBC statements.
+*/
 import java.sql.*;
 import java.util.Scanner;
 
@@ -8,16 +10,33 @@ public class Connect {
 	public static void main(String[] args) {
 		String username;
 		String password;
+		Scanner input = new Scanner(System.in);
 		
-		//This code does not work yet
+		//We will create a user account to access the database test
+		System.out.println("Please create an account.");
+		System.out.print("Username: ");
+		username = input.next();
+		System.out.print("Password: ");
+		password = input.next();
+		
+		//Using the JDBC Driver we will connect to the MySQL database
 		Class.forName("JDBCDriverClass");
 		Connection connection = DriverManager.getConnection
-				("jdbc:mysql://localhost/javaproject", username, password );
+				("jdbc:mysql://localhost/test", username, password );
 		
+		//Make a statement and carry out statement to make a new user
 		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery();
+		statement.executeUpdate("create user '"
+				+ username + "'@'%' identified by '" + password + "';");
+		System.out.println("Account created");
+		
+		
+		//This part is not finished
+		//ResultSet resultSet = statement.executeQuery();
 
 		connection.close();
+
+		
 	}
 	
 	
