c8c96a9ce425c2d0fe72c713355e53d9ad19a0ba
Jessica Wijaya <Jessica Wijaya@DESKTOP-LS24TNU>
2019-09-18 11:29:06 -0500

    Updated class Connect

--- Java Database Project/src/Connect.java
+++ Java Database Project/src/Connect.java
@@ -1,12 +1,25 @@
 
 //In this program we will connect to the database
 import java.sql.*;
+import java.util.Scanner;
 
 public class Connect {
 
 	public static void main(String[] args) {
+		String username;
+		String password;
 		
+		//This code does not work yet
+		Class.forName("JDBCDriverClass");
+		Connection connection = DriverManager.getConnection
+				("jdbc:mysql://localhost/javaproject", username, password );
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery();
 
+		connection.close();
 	}
+	
+	
 
 }
