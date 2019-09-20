8fbcfffed47c6eb86fff8f39f1f673f45152c28f
Jessica Wijaya <Jessica Wijaya@DESKTOP-LS24TNU.attlocal.net>
2019-09-20 17:49:57 -0500

    Updated User class

--- Java Database Project/src/User.java
+++ Java Database Project/src/User.java
@@ -3,30 +3,47 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class User {
+	private int customerId;				//Unique customer id
 	private String firstName;	//First name of customer
 	private String lastName;	//Last name of customer
+	private String phoneNumber;	
+	private String email;
 	
 	//Main method
 	public static void main(String[] args) {
 		User customer = new User();
-		customer.inputName();
+		customer.inputUserInfo();
 		
 		return;
 	}
 	
 	//This method asks the user for their name and assigns
 	//the strings to firstName and lastName
-	public void inputName() {
+	public void inputUserInfo() {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Please enter your name.");
 		System.out.print("First name: ");
 		firstName = input.next();
 		System.out.print("Last name: ");
 		lastName = input.next();
+		System.out.print("Phone number: ");
+		phoneNumber = input.next();
+		System.out.print("Email: ");
+		email = input.next();
+		customerId = createId();
 		insertData();
 		
 		input.close();
 		System.out.println("Program end");
+	}
+	
+	//This method returns a unique id number for each new customer
+	public int createId() {
+		//Search database user table for already taken id numbers
+		
+		int id=0;
+		
+		return id;
 	}
 	
 	//This method creates a connection to the database
@@ -46,12 +63,17 @@ public class User {
 	private void insertData() {
 		try {
 			Connection conn = connect();
-			PreparedStatement stmt = conn.prepareStatement("INSERT INTO user(firstName, lastName) VALUES(?, ?);");
-			stmt.setString(1, firstName);
-			stmt.setString(2, lastName);
+			PreparedStatement stmt = conn.prepareStatement("INSERT INTO user" +
+			"(customerId, firstName, lastName, phoneNumber, email) VALUES(?, ?, ?, ?, ?);");
+			stmt.setInt(1, customerId);
+			stmt.setString(2, firstName);
+			stmt.setString(3, lastName);
+			stmt.setString(4, phoneNumber);
+			stmt.setString(5, email);
 			
 			stmt.executeUpdate();
 			System.out.println("Data stored successfully");
+			conn.close();
 		}
 		catch(SQLException exception) {
 			System.out.println(exception);
