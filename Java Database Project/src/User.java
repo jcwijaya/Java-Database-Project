4ab72023f2410c5faa4d3b4680f1f37633be9857
Jessica Wijaya <Jessica Wijaya@DESKTOP-LS24TNU>
2019-09-21 12:42:48 -0500

    Updated User class

--- Java Database Project/src/User.java
+++ Java Database Project/src/User.java
@@ -3,7 +3,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class User {
-	private int customerId;				//Unique customer id
+	private int customerId;		//Unique customer id
 	private String firstName;	//First name of customer
 	private String lastName;	//Last name of customer
 	private String phoneNumber;	
