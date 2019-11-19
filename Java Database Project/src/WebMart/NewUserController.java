df91990c184a5daa6548e065624795d5118d77a1
hw_12 <hw_12@10.82.68.137>
2019-11-19 14:02:06 -0600

    Importing JavaFX

--- Java Database Project/src/WebMart/NewUserController.java
+++ Java Database Project/src/WebMart/NewUserController.java
@@ -1,5 +1,13 @@
 package WebMart;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class NewUserController {
+	
 
 }
