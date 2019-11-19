3023f4223f7a511d0312d257e3e381752ff56317
hw_12 <hw_12@10.82.68.137>
2019-11-19 14:22:13 -0600

    Adding enter button

--- Java Database Project/src/WebMart/NewUserController.java
+++ Java Database Project/src/WebMart/NewUserController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 
 //this is the controller for the page that registers new users
 public class NewUserController {
-	@FXML private Label titleLabel; //page header
+	@FXML private Label randomID; 	//displays randomly generated employee ID
+	@FXML Button enterBtn; 			//enters data typed in by user and creates new employee in database
 
 }
