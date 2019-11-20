e7d3e8e5a807719d7c24601126833078ca5297a7
hw_12 <hw_12@192.168.100.157>
2019-11-19 20:45:09 -0600

    putting in text fields

--- Java Database Project/src/WebMart/NewUserController.java
+++ Java Database Project/src/WebMart/NewUserController.java
@@ -9,7 +9,10 @@ import javafx.scene.control.TextField;
 
 //this is the controller for the page that registers new users
 public class NewUserController {
-	@FXML private Label randomID; 	//displays randomly generated employee ID
-	@FXML Button enterBtn; 			//enters data typed in by user and creates new employee in database
-	
+	@FXML private Label randomID; 		//displays randomly generated employee ID
+	@FXML Button enterBtn; 				//enters data typed in by user and creates new employee in database
+	@FXML private TextField firstTxt;	//text field to enter first name
+	@FXML private TextField lastTxt;	//text field to enter last name
+	@FXML private TextField phoneTxt;	//text field to enter phone number
+	@FXML private TextField emailTxt;	//text field to enter email
 }
