5b19f7fc222fa55bfb46a39c3018e29cbb9a4717
hw_12 <hw_12@192.168.100.157>
2019-11-19 20:47:54 -0600

    put in password fields

--- Java Database Project/src/WebMart/NewUserController.java
+++ Java Database Project/src/WebMart/NewUserController.java
@@ -9,10 +9,12 @@ import javafx.scene.control.TextField;
 
 //this is the controller for the page that registers new users
 public class NewUserController {
-	@FXML private Label randomID; 		//displays randomly generated employee ID
-	@FXML Button enterBtn; 				//enters data typed in by user and creates new employee in database
-	@FXML private TextField firstTxt;	//text field to enter first name
-	@FXML private TextField lastTxt;	//text field to enter last name
-	@FXML private TextField phoneTxt;	//text field to enter phone number
-	@FXML private TextField emailTxt;	//text field to enter email
+	@FXML private Label randomID; 			//displays randomly generated employee ID
+	@FXML Button enterBtn; 					//enters data typed in by user and creates new employee in database
+	@FXML private TextField firstTxt;		//text field to enter first name
+	@FXML private TextField lastTxt;		//text field to enter last name
+	@FXML private TextField phoneTxt;		//text field to enter phone number
+	@FXML private TextField emailTxt;		//text field to enter email
+	@FXML private PasswordField passTxt;	//password field to enter password
+	@FXML private PasswordField rePassTxt;	//password field to reenter password
 }
