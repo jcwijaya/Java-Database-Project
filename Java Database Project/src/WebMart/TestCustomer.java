c42ea99e2313215897d762f812cd788e1129aa88
hw_12 <hw_12@192.168.100.157>
2019-11-29 14:25:22 -0600

    NewUser Connection

--- Java Database Project/src/WebMart/TestCustomer.java
+++ Java Database Project/src/WebMart/TestCustomer.java
@@ -63,10 +63,12 @@ public class TestCustomer{
 			/*
 			Inventory item = new Inventory(productCode, category.trim(), productName.trim(), price, stock);
 			item.insert();
-*/
+
 			System.out.println("Insertion successful.");
 			}
 			scanner.close();
+			
+			
 		}
 		catch(FileNotFoundException e) {
 			e.printStackTrace();
